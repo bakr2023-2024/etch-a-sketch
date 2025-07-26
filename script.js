80a5f27f2ac8a43e7974b236afb85ffd76efb5d2
bakr2023-2024 <bakr2023.2024@gmail.com>
2025-07-26 06:17:19 +0300

    Add dynamic canvas setup feature

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 let pixelSize = 16;
 const canvas = document.querySelector("#container");
-document.addEventListener("DOMContentLoaded", () => {});
+const setPixelSizeBtn = document.querySelector("#setPixelSizeBtn");
 const setPixelSize = () => {
   pixelSize = Math.max(Number(prompt("Enter pixel size (max 100):")), 100);
 };
+const setupCanvas = () => {
+  const { width, height } = canvas.getBoundingClientRect();
+  const pixel = document.createElement("div");
+  const pixelWidth = width / pixelSize;
+  const pixelHeight = height / pixelSize;
+  pixel.setAttribute(
+    "style",
+    `width:${pixelWidth}px;height:${pixelHeight}px;background-color:white`
+  );
+  for (let i = 0; i < pixelSize; i++)
+    for (let j = 0; j < pixelSize; j++) canvas.appendChild(pixel);
+  pixel.addEventListener("hover", (e) => {
+    e.target.style.backgroundColor = "green";
+  });
+};
+document.addEventListener("DOMContentLoaded", () => {
+  setupCanvas();
+});
+setPixelSizeBtn.addEventListener("click", () => {
+  setPixelSize();
+  setupCanvas();
+});
