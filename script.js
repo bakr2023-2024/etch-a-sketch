8e6c88ea64f910bf63fde1e94b49c8dc5d8c3dbb
bakr2023-2024 <bakr2023.2024@gmail.com>
2025-07-26 06:35:33 +0300

    Add drawing over canvas feature

--- script.js
+++ script.js
@@ -15,7 +15,9 @@ const setupCanvas = () => {
         "style",
         `width: ${pixelWidth}px;height: ${pixelHeight}px;`
       );
-
+      pixel.addEventListener("mouseenter", (e) => {
+        e.target.style.backgroundColor = "green";
+      });
       canvas.appendChild(pixel);
     }
   }
