bc8d8730bdecf3f3b94d0453f8ea80c0c2c60331
bakr2023-2024 <bakr2023.2024@gmail.com>
2025-07-26 06:33:39 +0300

    Add dynamic canvas setup feature

--- script.js
+++ script.js
@@ -2,27 +2,33 @@ let pixelSize = 16;
 const canvas = document.querySelector("#container");
 const setPixelSizeBtn = document.querySelector("#setPixelSizeBtn");
 const setPixelSize = () => {
-  pixelSize = Math.max(Number(prompt("Enter pixel size (max 100):")), 100);
+  pixelSize = Math.min(Number(prompt("Enter pixel size (max 100):")), 100);
 };
 const setupCanvas = () => {
   const { width, height } = canvas.getBoundingClientRect();
-  const pixel = document.createElement("div");
   const pixelWidth = width / pixelSize;
   const pixelHeight = height / pixelSize;
-  pixel.setAttribute(
-    "style",
-    `width:${pixelWidth}px;height:${pixelHeight}px;background-color:white`
-  );
-  for (let i = 0; i < pixelSize; i++)
-    for (let j = 0; j < pixelSize; j++) canvas.appendChild(pixel);
-  pixel.addEventListener("hover", (e) => {
-    e.target.style.backgroundColor = "green";
-  });
+  for (let i = 0; i < pixelSize; i++) {
+    for (let j = 0; j < pixelSize; j++) {
+      const pixel = document.createElement("div");
+      pixel.setAttribute(
+        "style",
+        `width: ${pixelWidth}px;height: ${pixelHeight}px;`
+      );
+      pixel.addEventListener("mouseenter", (e) => {
+        e.target.style.backgroundColor = "green";
+      });
+      canvas.appendChild(pixel);
+    }
+  }
 };
+
 document.addEventListener("DOMContentLoaded", () => {
   setupCanvas();
 });
 setPixelSizeBtn.addEventListener("click", () => {
+  canvas.innerHTML = "";
   setPixelSize();
+  console.log(pixelSize);
   setupCanvas();
 });
